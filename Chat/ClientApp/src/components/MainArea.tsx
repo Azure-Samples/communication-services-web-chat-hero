a8211f3271f9ede92f5c524a0d769a4d8f7ada1c
Minnie Liu <peiliu@microsoft.com>
2021-06-08 15:34:57 -0700

    Add room tiles to main area

--- Chat/ClientApp/src/components/MainArea.tsx
+++ Chat/ClientApp/src/components/MainArea.tsx
@@ -1,7 +1,8 @@
 import React  from 'react';
-import { Image } from '@fluentui/react';
+import { DefaultButton, Image, Stack } from '@fluentui/react';
 import defaultImg from '../assets/default.png';
-import { staticImageStyle, staticAreaStyle} from './styles/ChatScreen.styles';
+import { staticImageStyle, staticAreaStyle } from './styles/ChatScreen.styles';
+import { tilesStackStyles, tilesStackTokens, tileStyle } from './styles/RoomTile.styles';
 
 export default (props: any): JSX.Element => {
   const imageProps = { src: defaultImg.toString() };
@@ -14,8 +15,12 @@ export default (props: any): JSX.Element => {
       />
       <h2>Event Description</h2>
       Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      <h2>Upcoming Sessions</h2>
-      Coming soon...
+          <h2>Upcoming Sessions</h2>
+          <Stack horizontal horizontalAlign="space-evenly" styles={tilesStackStyles} tokens={tilesStackTokens}>
+              <DefaultButton className={tileStyle} text="Global All Hands" />
+              <DefaultButton className={tileStyle} text="Product Demos" />
+              <DefaultButton className={tileStyle} text="Ask Me Anything" />
+          </Stack>
     </div>
   );
 };
---
+++ Chat/ClientApp/src/components/styles/RoomTile.styles.ts
@@ -0,0 +1,27 @@
+﻿import { IStackStyles, IStackTokens, mergeStyles } from '@fluentui/react';
+
+const tileStyle = mergeStyles({
+    width: '13rem',
+    height: '13rem',
+    borderRadius: '25px',
+    fontSize: '20px',
+    color: '#0078d4',
+});
+
+const tilesStackStyles: IStackStyles = {
+    root: {
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'flex-start',
+    },
+}
+
+const tilesStackTokens: IStackTokens = {
+    childrenGap: 25,
+};
+
+export {
+    tilesStackStyles,
+    tilesStackTokens,
+    tileStyle
+};
