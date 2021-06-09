97ed192bfb6cd76ba5008ecf3813cc3cf6feb85a
JoshuaLai <9044372+JoshuaLai@users.noreply.github.com>
2021-06-08 17:07:38 -0700

    [LeftPane] adding new props to screen and adding new menu button (#2)
    * [LeftPane] adding new props to screen and adding new menu button
    
    * Revert file
    
    * Update Chat.csproj
    
    * [LeftPane] Adding funcationality for the tabs to update the main area

--- Chat/ClientApp/src/components/MainArea.tsx
+++ Chat/ClientApp/src/components/MainArea.tsx
@@ -1,26 +1,53 @@
-import React  from 'react';
+import React, { useState }  from 'react';
 import { DefaultButton, Image, Stack } from '@fluentui/react';
 import defaultImg from '../assets/default.png';
 import { staticImageStyle, staticAreaStyle } from './styles/ChatScreen.styles';
 import { tilesStackStyles, tilesStackTokens, tileStyle } from './styles/RoomTile.styles';
 
-export default (props: any): JSX.Element => {
+export interface MainScreenProps {
+  contents: string 
+}
+
+export default (props: MainScreenProps): JSX.Element => {
   const imageProps = { src: defaultImg.toString() };
-  return (
-    <div className={staticAreaStyle}>
-      <Image
-        className={staticImageStyle}
-        alt="Default Event Image"
-        {...imageProps}
-      />
-      <h2>Event Description</h2>
-      Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  const { contents } = props;
+
+  const getComponent = () => {
+    if (contents === 'welcome') {
+        return (
+          <div className={staticAreaStyle}>
+          <Image
+            className={staticImageStyle}
+            alt="Default Event Image"
+            {...imageProps}
+          />
+          <h2>Event Description</h2>
+          Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           <h2>Upcoming Sessions</h2>
           <Stack horizontal horizontalAlign="space-evenly" styles={tilesStackStyles} tokens={tilesStackTokens}>
               <DefaultButton className={tileStyle} text="Global All Hands" />
               <DefaultButton className={tileStyle} text="Product Demos" />
               <DefaultButton className={tileStyle} text="Ask Me Anything" />
           </Stack>
-    </div>
-  );
+        </div>
+        );
+    } else if (contents === 'attendees') {
+      return (
+        <div className={staticAreaStyle}>
+        <h2>Attendees</h2>
+        Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      );
+
+    } else {
+      return (
+        <div className={staticAreaStyle}>
+        <h2>ERROR</h2>
+        Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      );
+    }
+  }
+  
+  return getComponent()
 };
