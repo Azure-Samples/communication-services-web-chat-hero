47fe46817d381b125ab71749107fc631565d2ee4
Paola Vicencio <pvicencio@microsoft.com>
2021-06-14 14:56:49 -0500

    fist attempt to add azure mp

--- Chat/ClientApp/src/components/Stream.tsx
+++ Chat/ClientApp/src/components/Stream.tsx
@@ -1,11 +1,14 @@
 ﻿import { ActionButton, FontIcon, IIconProps, Stack } from '@fluentui/react';
 import React from 'react';
 import { streamIconStyle, streamMainStyle, streamTextStyle } from './styles/Stream.styles';
+import AzureMP from './mp-component'
 
 export default (props: any): JSX.Element => {
   return (
     <div className={streamMainStyle}>
        <iframe height="100%" width="100%" src="https://www.youtube.com/embed/gy480NqX7yU?autoplay=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+       <h2>Azure Media Player</h2>
+       <AzureMP></AzureMP>
     </div>
   );
 };
