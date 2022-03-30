e83a4dffefb9d1b11fb259efd08c58dee941fc74
Prathmesh Prabhu <82062616+prprabhu-ms@users.noreply.github.com>
2022-03-29 17:02:23 -0700

    Remove unsupported button to open participant pane (#42)

--- Chat/src/app/ChatHeader.tsx
+++ Chat/src/app/ChatHeader.tsx
@@ -3,7 +3,6 @@
 
 import React from 'react';
 import { DefaultButton, Icon, IconButton, mergeStyles, Stack } from '@fluentui/react';
-import { People20Filled, People20Regular } from '@fluentui/react-icons';
 import {
   buttonWithIconStyles,
   chatHeaderContainerStyle,
@@ -12,7 +11,6 @@ import {
   leaveButtonStyle,
   leaveIcon,
   leaveIconStyle,
-  paneButtonContainerStyle,
   smallLeaveButtonContainerStyle
 } from './styles/ChatHeader.styles';
 import { useTheme } from '@azure/communication-react';
@@ -30,13 +28,6 @@ export const ChatHeader = (props: ChatHeaderProps): JSX.Element => {
 
   return (
     <Stack horizontal={true} verticalAlign={'center'} horizontalAlign="end" className={chatHeaderContainerStyle}>
-      <div className={paneButtonContainerStyle}>
-        <IconButton
-          onRenderIcon={() => (props.isParticipantsDisplayed ? <People20Filled /> : <People20Regular />)}
-          className={mergeStyles({ color: theme.palette.neutralPrimaryAlt })}
-          onClick={() => props.setHideParticipants(props.isParticipantsDisplayed)}
-        />
-      </div>
       <DefaultButton
         className={mergeStyles(largeLeaveButtonContainerStyle, leaveButtonStyle, {
           color: theme.palette.neutralPrimaryAlt
